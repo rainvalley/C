8be27ba9d97d71621268ccf7247b1b7bf61f6cf8
Hu Yadong <201916050318@stu.haut.edu.cn>
2020-04-25 11:03:26 +0800

    不会写，注释掉

--- general.c
+++ general.c
@@ -45,61 +45,61 @@ void Check_dorm(char* dorm_id)
 	Query_stu(1, dorm_id);
 }
 
-int Check_blank(char* dorm_id)
-{
-	struct student {
-		char id[15];
-		char name[5];
-		char phone[15];
-		char dorm_id[10];
-		char bed_id[2];
-		char head_id[15];
-		char head_name[5];
-		char head_phone[15];
-	};
-	struct student stu;
-	int bed_id[6];//记录已有床位信息
-	int blank_bed_id[6];//记录空床信息
-	int count = 0;//循环计数，并记录bed_id长度
-	int count_blank = 0;
-	int flag = 0;//ch与bed_id匹配与否
-	int flag_check = 0;
-	FILE* fp = fopen("data.txt", "r+");
-	//将文件内与宿舍号匹配的数据读入bed_id
-	while (fscanf(fp, "%s %s %s %s %s %s %s %s", stu.id, stu.name, stu.phone, stu.dorm_id, stu.bed_id, stu.head_id, stu.head_name, stu.head_phone) != -1)
-	{
-		if (strcmp(dorm_id, stu.dorm_id) == 0)
-		{
-			sscanf(stu.bed_id, "%d", &bed_id[count]);//将床号重定向为int类型，并储存到bed_id内
-			flag_check = 1;
-			count++;
-		}
-	};
-
-	if (flag_check==0)
-	{
-		return 0;
-	}
-	else
-	{
-		printf("%d\n", count);
-		for (int i = 1; i <= 6-count; i++)
-		{
-			for (int j = 0; j < count; j++)
-			{
-				if (bed_id[j] == i)
-				{
-					flag = 1;
-				}
-			}
-			if (flag==0)
-			{
-				blank_bed_id[count_blank] = i;
-				printf("%d\n", bed_id[count_blank]);
-				count_blank++;
-			}
-			flag = 0;
-		}
-		return 1;
-	}
-}
+//int Check_blank(char* dorm_id)
+//{
+//	struct student {
+//		char id[15];
+//		char name[5];
+//		char phone[15];
+//		char dorm_id[10];
+//		char bed_id[2];
+//		char head_id[15];
+//		char head_name[5];
+//		char head_phone[15];
+//	};
+//	struct student stu;
+//	int bed_id[6];//记录已有床位信息
+//	int blank_bed_id[6];//记录空床信息
+//	int count = 0;//循环计数，并记录bed_id长度
+//	int count_blank = 0;
+//	int flag = 0;//ch与bed_id匹配与否
+//	int flag_check = 0;
+//	FILE* fp = fopen("data.txt", "r+");
+//	//将文件内与宿舍号匹配的数据读入bed_id
+//	while (fscanf(fp, "%s %s %s %s %s %s %s %s", stu.id, stu.name, stu.phone, stu.dorm_id, stu.bed_id, stu.head_id, stu.head_name, stu.head_phone) != -1)
+//	{
+//		if (strcmp(dorm_id, stu.dorm_id) == 0)
+//		{
+//			sscanf(stu.bed_id, "%d", &bed_id[count]);//将床号重定向为int类型，并储存到bed_id内
+//			flag_check = 1;
+//			count++;
+//		}
+//	};
+//
+//	if (flag_check==0)
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		printf("%d\n", count);
+//		for (int i = 1; i <= 6; i++)
+//		{
+//			for (int j = 0; j < count; j++)
+//			{
+//				if (bed_id[j] == i)
+//				{
+//					flag = 1;
+//				}
+//			}
+//			if (flag==0)
+//			{
+//				blank_bed_id[count_blank] = i;
+//				printf("%d\n", bed_id[count_blank]);
+//				count_blank++;
+//			}
+//			flag = 0;
+//		}
+//		return 1;
+//	}
+//}
--- main.c
+++ main.c
@@ -20,45 +20,6 @@ struct student {
 
 int main()
 {
-	Check_blank("A02S412");
-	//char str[32];
-	//char sha256[65];
-	//scanf("%s", str);
-	//hash(str, sizeof(str) - 1,sha256);  // sizeof()计算的结果包含了末尾的'\0'应减1
-	//puts(sha256);
-	//return 0;
-	/*char id[20];
-	char old_passwd[50];
-	char new_passwd[32];
-	scanf("%s %s %s", id, old_passwd,new_passwd);
-	Update_info(id, old_passwd,new_passwd);*/
-	/*Reset_password("2098");*/
-	/*while (1)
-	{
-		scanf("%s %s", id, passwd);
-		if (Check_permission(id, passwd, 1))
-		{
-			printf("Correct!");
-		}
-		else
-		{
-			printf("Wrong!");
-		}
-	}*/
-	/*Create_general_user(id, passwd);*/
-	/*Delete_general_user(id);*/
-	//Reset_password(id);
-	//Delete_stu(id);
-	/*int method;
-	char info[20];
-	while (1)
-	{
-		scanf("%d %s", &method, info);
-		Query_stu(method, info);
-	}*/
-	/*struct student stu;
-	scanf("%s %s %s %s %s %s %s %s", stu.id, stu.name, stu.phone, stu.dorm_id, stu.bed_id, stu.head_id, stu.head_name, stu.head_phone);
-	Update_stu(stu);*/
-	/*Create_stu(stu);*/
-	//View_data();
+
+	
 }
