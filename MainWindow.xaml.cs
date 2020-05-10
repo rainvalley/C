8c38051a8bd0225c419fed4865a0aee3550a93b5
HuYadong <HuYadong@DESKTOP-SSB4J9C>
2020-05-10 14:21:42 +0800

    修正DLL库的错误与Hash函数在登陆界面的使用

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,33 +22,46 @@ namespace 宿舍管理系统_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        [DllImport("Check.dll", EntryPoint = "Check_permission", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("sys.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int Check_permission(string id, string password, int type);
+        [DllImport("sys.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern IntPtr hash(string str);
         public int times = 0;
         public MainWindow()
         {
             InitializeComponent();
+            //直接跳转admin页面
+            //admin newpage = new admin();
+            //newpage.Show();
+            //this.Close();
         }
         private void Button_Login(object sender, RoutedEventArgs e)
         {
             string username = user.Text;
             string passwd = password.Password;
             int type = box.SelectedIndex;
-            if (Check_permission(username, passwd, type)==0)
+            string passwd_hash = Marshal.PtrToStringAnsi(hash(passwd));
+            if (Check_permission(username, passwd_hash, type) == 0)
             {
                 MessageBox.Show("账号或密码错误");
                 times++;
-                if(times>=5)
+                if (times >= 5)
                 {
                     MessageBox.Show("您已累计输入错误5次，请稍后再试。");
                     this.Close();
                     Environment.Exit(0);
                 }
             }
-            else if (Check_permission(username, passwd, type) == 1)
+            else if (Check_permission(username, passwd_hash, type) == 1 && type == 0) 
             {
                 admin newpage = new admin();
                 newpage.Show();
+                this.Close();
+            }
+            else if (Check_permission(username, passwd_hash, type) == 1 && type == 1)
+            {
+                general newpage_general = new general();
+                newpage_general.Show();
                 this.Close();
             }
             else
--- admin.xaml.cs
+++ admin.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
 
 namespace 宿舍管理系统_WPF
 {
@@ -19,10 +20,99 @@ namespace 宿舍管理系统_WPF
     /// </summary>
     public partial class admin : Window
     {
+        [DllImport("sys.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Create_general_user(string id,string password);
+        [DllImport("sys.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Delete_general_user(string id);
+        [DllImport("sys.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Reset_password(string id);
+
+        string[] user=new string[] {"创建账号","删除账号","重置账号" };
+        string[] stu = new string[] { "新建住宿信息", "更新住宿信息", "删除住宿信息", "查询住宿信息", "浏览住宿信息", "删除毕业生信息" };
         public admin()
         {
             InitializeComponent();
+            typebox.Items.Add("用户信息");
+            typebox.Items.Add("学生住宿信息");
         }
 
+        public static implicit operator admin(general v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void typeboxchanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (typebox.SelectedIndex == 0)
+            {
+                infobox.Items.Clear();
+                for (int i = 0; i < 3; i++)
+                {
+                    infobox.Items.Add(user[i]);
+                }
+            }
+            else if (typebox.SelectedIndex == 1)
+            {
+                infobox.Items.Clear();
+                for (int i = 0; i < 6; i++)
+                {
+                    infobox.Items.Add(stu[i]);
+                }
+            }
+        }
+
+        public void infoboxchanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (typebox.SelectedIndex == 0)
+            {
+                if (infobox.SelectedIndex == 0)
+                {
+                    MessageBox.Show("00");
+                }
+                else if (infobox.SelectedIndex == 1)
+                {
+                    MessageBox.Show("01");
+                }
+                else if (infobox.SelectedIndex == 2)
+                {
+                    MessageBox.Show("02");
+                }
+            }
+            else if (typebox.SelectedIndex == 1)
+            { 
+                switch (infobox.SelectedIndex)
+                {
+                    case 0:
+                        MessageBox.Show("10");
+                        break;
+                    case 1:
+                        MessageBox.Show("11");
+                        break;
+                    case 2:
+                        MessageBox.Show("12");
+                        break;
+                    case 3:
+                        MessageBox.Show("13");
+                        break;
+                    case 4:
+                        MessageBox.Show("14");
+                        break;
+                    case 5:
+                        MessageBox.Show("15");
+                        break;
+                }
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void passwd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
